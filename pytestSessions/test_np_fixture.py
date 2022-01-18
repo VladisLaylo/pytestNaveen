08f430bcf7338831989132b707b08ecf29ba9078
Vladis Brightpattern <vladis.laylo@brightpattern.com>
2022-01-18 18:15:42 +0300

    continue keep fresh

--- pytestSessions/test_np_fixture.py
+++ pytestSessions/test_np_fixture.py
@@ -1,32 +1,29 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import pytest
 
 driver = None
 
 
-def setup_module(module):
+@pytest.fixture(scope='module')
+def init_driver():
     global driver
+    print("------------setup------------")
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(10)
     driver.delete_all_cookies()
     driver.get('https://vladis03.bugfocus.com/numberprovisioning/provider')
 
-
-def teardown_module(module):
+    yield
+    print("----------tear down----------")
     driver.quit()
 
 
-def test_np_title():
+def test_np_title(init_driver):
     assert driver.title == "Phone number provisioning"
+    print("Title found: " + driver.title)
 
 
-def test_np_urls():
+def test_np_urls(init_driver):
     assert driver.current_url == "https://vladis03.bugfocus.com/numberprovisioning/provider"
-
-
-"""
-Setup generate to HTML report:
-1. pip install pytest-html
-2. py.test test_webpage_np.py -v -s --html=np_test_report.html
-* if you want to change css styles in project folder: assets/style.css
-"""
+    print("URL found: " + driver.current_url)
