94496db924151b08b9a8a804926d227884b80d47
Vladis Brightpattern <vladis.laylo@brightpattern.com>
2022-01-18 19:19:48 +0300

    continue keep fresh

--- pytestSessions/test_np_fixture.py
+++ pytestSessions/test_np_fixture.py
@@ -45,9 +45,19 @@ def test_click_on_ph():
     print("You are on Orders page")
 
 
-def test_expand_tenants():
+def test_expand_tenants_menu_in_orders():  # didn't find options_list
     time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="appContainer"]/div/div[2]/div[1]/div[4]/div/div/div[2]/div/i').click()
+    options_list = driver.find_elements(By.XPATH, '//*[@id="appContainer"]/div/div[2]/div[1]/div[4]/div/div/div['
+                                                  '2]/div/div[2]')  # find all prediction keywords
+
+    for ele in options_list:  # print all prediction keywords
+        print(ele.text)
+        if ele.text == "a3Tenant61.com":  # click on this keyword
+            ele.click()
+            break  # break the loop
+    print("You did expand the menu and select tenant a3Tenant61")
+
 
 """
 pip install fixture
