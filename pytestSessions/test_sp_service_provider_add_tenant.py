a85edd74d66f0d8a240cfc2dca56482d0535fcf1
Seondes <seondes@gmail.com>
2022-01-26 20:54:02 +0300

    From MacOS

--- pytestSessions/test_sp_service_provider_add_tenant.py
+++ pytestSessions/test_sp_service_provider_add_tenant.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
