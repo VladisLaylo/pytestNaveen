c1f9d113ac939b4550fb78b5aac0401850298597
Seondes <seondes@gmail.com>
2022-02-06 17:33:29 +0300

    From MacOS

--- pytestSessions/ElementActionsConcept.py
+++ pytestSessions/ElementActionsConcept.py
@@ -8,19 +8,21 @@ import time
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.implicitly_wait(10)
 driver.get('https://classic.crmpro.com/')
+time.sleep(4)
 
 username_ele = driver.find_element(By.NAME, 'username')
 password_ele = driver.find_element(By.NAME, 'password')
 login_button_ele = driver.find_element(By.XPATH, "//input[@type='submit']")
 
 act_chains = ActionChains(driver)
-act_chains.send_keys(username_ele, 'batchautomation').perform()
-act_chains.send_keys(password_ele, 'Test@12345').perform()
-act_chains.click(login_button_ele)
 
+act_chains.send_keys_to_element(username_ele, 'batchautomation')  # no need .perform() with send_keys_to_element
+act_chains.send_keys_to_element(password_ele, 'Test@12345')  # to avoid send keys multiple times
+act_chains.click(login_button_ele).perform()
 
 time.sleep(3)
 driver.quit()
+
 '''
 Part of the lesson:
 https://youtu.be/pFLmQ4Vo9Co
