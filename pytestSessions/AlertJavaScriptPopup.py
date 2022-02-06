58a32a8b106852dddb4e7f45371248de99960a3e
Seondes <seondes@gmail.com>
2022-02-06 20:32:23 +0300

    From MacOS

--- pytestSessions/AlertJavaScriptPopup.py
+++ pytestSessions/AlertJavaScriptPopup.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver = webdriver.Chrome(service.Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.implicitly_wait(10)
 
 driver.get('https://mail.rediff.com/cgi-bin/login.cgi')
